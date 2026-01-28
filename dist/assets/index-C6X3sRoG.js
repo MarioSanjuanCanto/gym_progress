7779cb4dca3de83a068447fe1a832265a634aa1e
Mario Sanjuan Canto <msancan3@upv.edu.es>
2026-01-28 21:18:07 +0100

    fix: github pages 2

--- dist/assets/index-C6X3sRoG.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-DaIZ6e2b.js","assets/index-DeT2M_mL.js"])))=>i.map(i=>d[i]);
-import{_ as r}from"./index-BjCHVque.js";import{registerPlugin as o}from"./index-DeT2M_mL.js";const i=o("Preferences",{web:()=>r(()=>import("./web-DaIZ6e2b.js"),__vite__mapDeps([0,1])).then(e=>new e.PreferencesWeb)});export{i as Preferences};
